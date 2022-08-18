5930a983b8640ee922e2b20797cf4b0a99b8c935
IonutPad <isdark23@yahoo.com>
2022-08-18 16:31:33 +0300

    added Fluent Validation

--- IssueTracker/Validation/ParticipantUpdateRequestValidation.cs
+++ IssueTracker/Validation/ParticipantUpdateRequestValidation.cs
@@ -1,9 +1,16 @@
-﻿using Models.Request;
+﻿using FluentValidation;
+using Models.Request;
 
 namespace Validation
 {
-    public class ParticipantUpdateRequestValidation
+    public class ParticipantUpdateRequestValidation : AbstractValidator<ParticipantUpdateRequest>
     {
+        public ParticipantUpdateRequestValidation()
+        {
+            RuleFor(x => x).NotNull().WithMessage("It should not be null");
+            RuleFor(x => x.Id).GreaterThan(0).WithMessage("Ids should be greater than 0!");
+            RuleFor(x => (int)x.RoleId).GreaterThan(0).WithMessage("Ids should be greater than 0!");
+        }
         public static bool IsValid(ParticipantUpdateRequest participant)
         {
             if(participant == null) return false;
