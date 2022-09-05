31ee71c1cbc523027949ca7f99c47ce8c396ff28
Ioan Iusan <ioan.iusan@lateral-inc.com>
2022-09-05 19:59:01 +0300

    Added FileSystemServiceExtensions

---
+++ IssueTracker/IssueTracker.FileSystem/Registration/FileSystemServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using IssueTracker.FileSystem.Repository;
+using IssueTracker.FileSystem.Repository.IRepository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IssueTracker.FileSystem;
+
+public static class FileSystemServiceExtensions
+{
+    public static IServiceCollection AddFileSystemServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IFileProvider, FileProvider>();
+        return services;
+    }
+}
--- IssueTracker/IssueTracker.FileSystem/Repository/FileProvider.cs
+++ IssueTracker/IssueTracker.FileSystem/Repository/FileProvider.cs
@@ -1,23 +1,12 @@
-﻿using IssueTracker.FileSystem.Models;
-using IssueTracker.FileSystem.Repository.IRepository;
+﻿using IssueTracker.FileSystem.Repository.IRepository;
 using Microsoft.Extensions.Configuration;
 
 namespace IssueTracker.FileSystem.Repository;
-public class FileProvider : IFileProvider
+internal class FileProvider : IFileProvider
 {
     private readonly IMetaDataProvider _metadata;
     private readonly IBolbData _bolb;
 
-    public static FileProvider GetFileProvider(IConfiguration config)
-    {
-        IConfigurationFactory cf = new ConfigurationFactory(config);
-        var metaDataConfig = (IMetaDataConfiguration)cf.Create<IMetaDataConfiguration>();
-        IMetaDataProvider metaDataProvider = new MetaData(metaDataConfig);
-        var blobConfig = (IBolbConfigurationFactory)cf.Create<IBolbConfigurationFactory>();
-        IBolbData blobData = new BolbData(blobConfig);
-        FileProvider fileProvider = new FileProvider(config, metaDataProvider, blobData);
-        return fileProvider;
-    }
     public FileProvider(IConfiguration config)
     {
         IConfigurationFactory cf = new ConfigurationFactory(config);
@@ -25,14 +14,6 @@ public class FileProvider : IFileProvider
         _metadata = new MetaData(metaDataConfig);
         var blobConfig = (IBolbConfigurationFactory)cf.Create<IBolbConfigurationFactory>();
         _bolb = new BolbData(blobConfig);
-    }
-    internal FileProvider(IConfiguration config, IMetaDataProvider metadata, IBolbData bolb)
-    {
-        IConfigurationFactory dataFactory = new ConfigurationFactory(config);
-        IMetaDataConfiguration metaDataConfig = (IMetaDataConfiguration)dataFactory.Create<IMetaDataConfiguration>();
-        IBolbConfigurationFactory bolbConfig = (IBolbConfigurationFactory)dataFactory.Create<IBolbConfigurationFactory>();
-        _metadata = metadata;
-        _bolb = bolb;
     }
 
     public async Task DeleteAsync(Models.File file)
