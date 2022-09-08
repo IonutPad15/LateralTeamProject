5a680d83cf149a9affdf0c1864bc376ca2207855
IonutPad <isdark23@yahoo.com>
2022-09-08 12:35:33 +0300

    2

--- IssueTracker/IssueTracker.FileSystem/Repository/IRepository/IBolbData.cs
+++ IssueTracker/IssueTracker.FileSystem/Repository/IRepository/IBolbData.cs
@@ -6,5 +6,6 @@ internal interface IBolbData
 {
     Task<IEnumerable<Models.File>> GetFilesAsync(IEnumerable<Models.File> files);
     Task UploadFileAsync(Models.File file);
+    Task<Stream> DownloadFileAsync(string link);
     Task<bool> DeleteFileAsync(string name);
 }
