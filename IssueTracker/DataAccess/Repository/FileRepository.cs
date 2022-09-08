19c127ff8706416b4761f81625d444ed104d6318
IonutPad <isdark23@yahoo.com>
2022-09-08 12:40:33 +0300

    3

--- IssueTracker/DataAccess/Repository/FileRepository.cs
+++ IssueTracker/DataAccess/Repository/FileRepository.cs
@@ -10,19 +10,22 @@ public class FileRepository : IFileRepository
     }
     public async Task<string?> AddAsync(Models.File entity)
     {
+        DateTime upate = DateTime.UtcNow;
         var result = await _db.SaveDataAndGetIdAsync<dynamic, string>("dbo.spFile_Insert", new
         {
             FileId = entity.FileId,
             Extension = entity.Extension,
             FileIssueId = entity.FileIssueId,
-            FileCommentId = entity.FileCommentId
+            FileCommentId = entity.FileCommentId,
+            Updated = upate
         });
         return result;
     }
 
     public async Task DeleteAsync(string fileId)
     {
-        await _db.SaveDataAsync("dbo.spFile_Delete", new { FileId = fileId });
+        DateTime upate = DateTime.UtcNow;
+        await _db.SaveDataAsync("dbo.spFile_Delete", new { FileId = fileId, Updated = upate });
     }
 
     public async Task<IEnumerable<Models.File>> GetByIssueIdAsync(int issueId)
