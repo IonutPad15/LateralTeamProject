daf5ea73e01852ed6b56c6ae5d64f4c45d609f16
IonutPad <isdark23@yahoo.com>
2022-09-08 13:44:44 +0300

    done

--- IssueTracker/IssueTracker.FileSystem/Repository/IRepository/IBolbData.cs
+++ IssueTracker/IssueTracker.FileSystem/Repository/IRepository/IBolbData.cs
@@ -1,4 +1,7 @@
-﻿namespace IssueTracker.FileSystem;
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("IssueTracker.UnitTest")]
+namespace IssueTracker.FileSystem;
 internal interface IBolbData
 {
     Task<IEnumerable<Models.File>> GetFilesAsync(IEnumerable<Models.File> files);
